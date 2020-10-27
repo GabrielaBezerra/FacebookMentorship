c1e4feb97187794709336cab354bceaac368a22e
Gabriela Bezerra <gabrieladecarvalhobezerra@gmail.com>
2020-10-27 18:40:24 -0300

    Delete to_lower_case.swift

--- Week 0/to_lower_case.swift
+++
@@ -1,7 +0,0 @@
-// Sorry to this challenger is easier in swift :c Please accept my pull request 🥺
-
-class Solution {
-    func toLowerCase(_ str: String) -> String {
-        return str.lowercased()
-    }
-}
