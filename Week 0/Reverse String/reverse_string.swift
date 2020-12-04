fe3a8de52852954d894f1e9441c178c389b7f58e
All3yp <noyalle@ualberta.ca>
2020-12-04 16:05:25 -0300

    Reverse String - Swift and Java - Week 0

---
+++ Week 0/Reverse String/reverse_string.swift
@@ -0,0 +1,14 @@
+// https://developer.apple.com/documentation/swift/array/2893281-swapat
+
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        var i = 0
+        var j = s.count-1
+        
+        while i < j {
+            s.swapAt(i,j)
+            i += 1
+            j -= 1
+        }
+    }
+}
